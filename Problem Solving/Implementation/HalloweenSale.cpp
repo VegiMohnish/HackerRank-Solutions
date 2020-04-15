f0675e9f2b0a82e17b5cf69b2e108da38db80ebb
Mohnish Vegi <vegimohnish@gmail.com>
2020-04-15 17:13:03 +0530

    Create HalloweenSale.cpp

---
+++ Problem Solving/Implementation/HalloweenSale.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int p,d,m,s;
+	int count=0;
+	cin >> p >> d >> m >> s;
+	
+	while(s>=p) {
+		count++;
+		s = s-p;
+		p = max(p-d,m);
+	}
+	cout << count;
+
+	return 0;
+
+}
