15fe208068707e2afb9061f868b4c57253b94ad1
Mohnish Vegi <vegimohnish@gmail.com>
2020-04-24 10:51:23 +0530

    Add files via upload

---
+++ Problem Solving/Implementation/DivisibleSumPairs.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,k,i,j;
+    cin >> n >> k;
+    int a[n];
+    for(i=0;i<n;i++)
+    	cin >> a[i];
+    int count=0;
+    for(i=0;i<n;i++) 
+        for(j=0;j<n;j++) 
+            if( i<j && (a[i]+a[j]) % k == 0) 
+                count++;
+    cout << count;
+    return 0;
+}
