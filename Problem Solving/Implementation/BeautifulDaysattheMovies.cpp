0c42e30cad9d7a43574ddef88d3c6e2622c7334a
Mohnish Vegi <vegimohnish@gmail.com>
2020-05-07 20:10:15 +0530

    Add files via upload

---
+++ Problem Solving/Implementation/BeautifulDaysattheMovies.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int reverse(int n) {
+	int rev=0,rem;
+	while(n!=0) {
+		rem = n%10;
+		rev = rev * 10 + rem;
+		n /= 10;
+	}
+	return rev;
+}
+
+
+int main() {
+	long int i,j,k,ir;
+	int count=0;
+	cin >> i >> j >> k;
+
+
+	for(int p=i;p<=j;p++) {
+			ir = reverse(p);
+
+		if(abs(p-ir)%k == 0)
+			count++;
+		
+	}
+	cout << count;
+	return 0;
+}
